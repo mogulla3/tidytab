2381ec419a431d26525f29af06f5ff4db8f5c136
mogulla3 <hs.iarn3@gmail.com>
2022-12-17 21:59:27 +0900

    Normalize url before compareing

--- src/main.ts
+++ src/main.ts
@@ -11,21 +11,34 @@ const sortTab = (tabA: chrome.tabs.Tab, tabB: chrome.tabs.Tab, order: string): n
         return 0;
     }
 
-    const tabAUrl: URL = new URL(tabA.url);
-    const tabBUrl: URL = new URL(tabB.url);
-    const domainAndPathA = `${tabAUrl.hostname}${tabAUrl.pathname}`;
-    const domainAndPathB = `${tabBUrl.hostname}${tabBUrl.pathname}`;
+    const urlA = normalizeUrl(tabA.url);
+    const urlB = normalizeUrl(tabB.url);
 
     if (order === "asc") {
-        if (domainAndPathA < domainAndPathB) return -1;
-        if (domainAndPathA > domainAndPathB) return 1;
+        if (urlA < urlB) return -1;
+        if (urlA > urlB) return 1;
     } else if (order === "desc") {
-        if (domainAndPathA < domainAndPathB) return 1;
-        if (domainAndPathA > domainAndPathB) return -1;
+        if (urlA < urlB) return 1;
+        if (urlA > urlB) return -1;
     }
 
     return 0;
 };
+
+// protocol (http, https) を削る
+// host の prefix `www.` を削る
+// searchParams を削る
+//
+// Example:
+// normalizeUrl("https://www.mogulla3.tech:9000/foo/bar/baz?q=100")
+// => "mogulla3.tech:9000/foo/bar/baz"
+const normalizeUrl = (urlStr: string): string => {
+    const url: URL = new URL(urlStr);
+
+    const host = url.host.startsWith("www.") ? url.host.slice(4) : url.host;
+
+    return `${host}${url.pathname}`;
+}
 
 const sortTabOrderByAsc = (tabA: chrome.tabs.Tab, tabB: chrome.tabs.Tab): number => {
     return sortTab(tabA, tabB, "asc");
