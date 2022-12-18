48ab07bdabc25beee92853e2c32e27d8873a0926
mogulla3 <hs.iarn3@gmail.com>
2022-12-18 21:13:08 +0900

    Fix indent_size

--- src/main.ts
+++ src/main.ts
@@ -3,51 +3,51 @@ import { sortTabOrderByAsc, sortTabOrderByDesc } from "./sort.js";
 import { extractTabIds } from "./utils.js";
 
 document.getElementById("sort_asc")?.addEventListener("click", (_event) => {
-    chrome.tabs.query({ currentWindow: true }, (tabs: chrome.tabs.Tab[]) => {
-        for (const [index, tabId] of extractTabIds(tabs.sort(sortTabOrderByAsc)).entries()) {
-            chrome.tabs.move(tabId, { index: index });
-        }
-    });
+  chrome.tabs.query({ currentWindow: true }, (tabs: chrome.tabs.Tab[]) => {
+    for (const [index, tabId] of extractTabIds(tabs.sort(sortTabOrderByAsc)).entries()) {
+      chrome.tabs.move(tabId, { index: index });
+    }
+  });
 });
 
 document.getElementById("sort_desc")?.addEventListener("click", (_event) => {
-    chrome.tabs.query({ currentWindow: true }, (tabs: chrome.tabs.Tab[]) => {
-        for (const [index, tabId] of extractTabIds(tabs.sort(sortTabOrderByDesc)).entries()) {
-            chrome.tabs.move(tabId, { index: index });
-        }
-    });
+  chrome.tabs.query({ currentWindow: true }, (tabs: chrome.tabs.Tab[]) => {
+    for (const [index, tabId] of extractTabIds(tabs.sort(sortTabOrderByDesc)).entries()) {
+      chrome.tabs.move(tabId, { index: index });
+    }
+  });
 });
 
 document.getElementById("remove_dup")?.addEventListener("click", (_event) => {
-    chrome.tabs.query({ currentWindow: true }, (tabs: chrome.tabs.Tab[]) => {
-        const urls: string[] = [];
-        const duplicatedTabIds: number[] = [];
+  chrome.tabs.query({ currentWindow: true }, (tabs: chrome.tabs.Tab[]) => {
+    const urls: string[] = [];
+    const duplicatedTabIds: number[] = [];
 
-        for (const tab of tabs) {
-            if (tab.id === undefined || tab.url === undefined) {
-                continue;
-            }
+    for (const tab of tabs) {
+      if (tab.id === undefined || tab.url === undefined) {
+        continue;
+      }
 
-            if (urls.includes(tab.url)) {
-                duplicatedTabIds.push(tab.id);
-                continue;
-            }
+      if (urls.includes(tab.url)) {
+        duplicatedTabIds.push(tab.id);
+        continue;
+      }
 
-            urls.push(tab.url);
-        }
+      urls.push(tab.url);
+    }
 
-        chrome.tabs.remove(duplicatedTabIds);
-    });
+    chrome.tabs.remove(duplicatedTabIds);
+  });
 });
 
 document.getElementById("remove_initial")?.addEventListener("click", (_event) => {
-    chrome.tabs.query({ url: "chrome://newtab/", currentWindow: true }, (tabs: chrome.tabs.Tab[]) => {
-        chrome.tabs.remove(extractTabIds(tabs));
-    });
+  chrome.tabs.query({ url: "chrome://newtab/", currentWindow: true }, (tabs: chrome.tabs.Tab[]) => {
+    chrome.tabs.remove(extractTabIds(tabs));
+  });
 });
 
 document.getElementById("only_current_tab")?.addEventListener("click", (_event) => {
-    chrome.tabs.query({ active: false, currentWindow: true }, (tabs: chrome.tabs.Tab[]) => {
-        chrome.tabs.remove(extractTabIds(tabs));
-    });
+  chrome.tabs.query({ active: false, currentWindow: true }, (tabs: chrome.tabs.Tab[]) => {
+    chrome.tabs.remove(extractTabIds(tabs));
+  });
 });
--- src/sort.ts
+++ src/sort.ts
@@ -1,6 +1,6 @@
 const enum SortOrder {
-    ASC,
-    DESC,
+  ASC,
+  DESC,
 }
 
 // - Remove protocol(http, https).
@@ -11,37 +11,37 @@ const enum SortOrder {
 // normalizeUrl(new URL("https://www.mogulla3.tech:9000/foo/bar/baz?q=100"))
 // => "mogulla3.tech:9000/foo/bar/baz"
 const normalizeUrl = (url: string): string => {
-    const urlObj = new URL(url);
-    const host = urlObj.host.startsWith("www.") ? urlObj.host.slice(4) : urlObj.host;
+  const urlObj = new URL(url);
+  const host = urlObj.host.startsWith("www.") ? urlObj.host.slice(4) : urlObj.host;
 
-    return `${host}${urlObj.pathname}`;
+  return `${host}${urlObj.pathname}`;
 };
 
 const sortTab = (tabA: chrome.tabs.Tab, tabB: chrome.tabs.Tab, sortOrder: SortOrder): number => {
-    if (tabA.url === undefined || tabB.url === undefined) {
-        return 0;
-    }
-
-    const urlA = normalizeUrl(tabA.url);
-    const urlB = normalizeUrl(tabB.url);
-
-    if (sortOrder === SortOrder.ASC) {
-        if (urlA < urlB) return -1;
-        if (urlA > urlB) return 1;
-    }
-
-    if (sortOrder === SortOrder.DESC) {
-        if (urlA < urlB) return 1;
-        if (urlA > urlB) return -1;
-    }
-
+  if (tabA.url === undefined || tabB.url === undefined) {
     return 0;
+  }
+
+  const urlA = normalizeUrl(tabA.url);
+  const urlB = normalizeUrl(tabB.url);
+
+  if (sortOrder === SortOrder.ASC) {
+    if (urlA < urlB) return -1;
+    if (urlA > urlB) return 1;
+  }
+
+  if (sortOrder === SortOrder.DESC) {
+    if (urlA < urlB) return 1;
+    if (urlA > urlB) return -1;
+  }
+
+  return 0;
 };
 
 export const sortTabOrderByAsc = (tabA: chrome.tabs.Tab, tabB: chrome.tabs.Tab): number => {
-    return sortTab(tabA, tabB, SortOrder.ASC);
+  return sortTab(tabA, tabB, SortOrder.ASC);
 };
 
 export const sortTabOrderByDesc = (tabA: chrome.tabs.Tab, tabB: chrome.tabs.Tab): number => {
-    return sortTab(tabA, tabB, SortOrder.DESC);
+  return sortTab(tabA, tabB, SortOrder.DESC);
 };
