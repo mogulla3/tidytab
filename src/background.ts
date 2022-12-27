af579d6babedc580988e46a12e7e895360db35e3
mogulla3 <hs.iarn3@gmail.com>
2022-12-27 09:44:11 +0900

    [wip] Cerate contextMenu

--- src/background.ts
+++ src/background.ts
@@ -1,1 +1,10 @@
 export {}
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    "id": "tidytab-context-menu",
+    "title": "tidytab Context Menu",
+    "contexts": ["page"],
+    "type": "normal",
+  });
+});
