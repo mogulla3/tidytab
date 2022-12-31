c6666fb563c48615ae887380c52eacc5ef58d43a
mogulla3 <hs.iarn3@gmail.com>
2022-12-31 16:01:49 +0900

    Consider pinned tabs

--- src/sortTab.ts
+++ src/sortTab.ts
@@ -50,7 +50,8 @@ export const sortTabsOrderByDesc = (tabs: chrome.tabs.Tab[]): chrome.tabs.Tab[] 
 };
 
 export const sortTabs = async (sortOrder: SortOrder) => {
-  const tabs = await chrome.tabs.query({ currentWindow: true, groupId: chrome.tabGroups.TAB_GROUP_ID_NONE });
+  const tabs = await chrome.tabs.query({ currentWindow: true, groupId: chrome.tabGroups.TAB_GROUP_ID_NONE, pinned: false });
+  const pinnedTabs = await chrome.tabs.query({ currentWindow: true, groupId: chrome.tabGroups.TAB_GROUP_ID_NONE, pinned: true });
   const tabGroups = await chrome.tabGroups.query({ windowId: chrome.windows.WINDOW_ID_CURRENT });
 
   let sortedTabs: chrome.tabs.Tab[] = [];
@@ -66,13 +67,13 @@ export const sortTabs = async (sortOrder: SortOrder) => {
 
   const tabIds = extractTabIds(sortedTabs);
   for (const [index, tabId] of tabIds.entries()) {
-    chrome.tabs.move(tabId, { index: index });
+    chrome.tabs.move(tabId, { index: index + pinnedTabs.length });
   }
 
   // tabGroup を move する際、内包するタブの件数を考慮して index を指定する必要がある.
   // tabGroup からタブの件数を取るのは面倒なため、先頭に移動させる `index = 0` を逆順に適用することでソートしている
   const tabGroupIds = extractTabGroupIds(sortedTabGroups);
   for (const tabGroupId of tabGroupIds.reverse()) {
-    chrome.tabGroups.move(tabGroupId, { index: 0 });
+    chrome.tabGroups.move(tabGroupId, { index: 0 + pinnedTabs.length });
   }
 };
